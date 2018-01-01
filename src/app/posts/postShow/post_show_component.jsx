b70df70e1bab587307eae6e167a52b1aea31dd8f
Adrian <adrianjewell91@gmail.com>
2017-12-31 23:06:38 -0800

    style

--- src/app/posts/postShow/post_show_component.jsx
+++ src/app/posts/postShow/post_show_component.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export const PostShowComponent = ({post}) => (
   <div className="post-index-item">
     <div className="post-index-author-info">
-      {post.publish_date}: {post.title} by {post.author}
+      {post.title} by <span className="author-span">{post.author}</span> on {post.publish_date}
     </div>
     <br/>
     <div className="post-index-content"
